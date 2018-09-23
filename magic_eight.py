973ccd396f65c3db1b1bd1d2aa52661c4e24d472
Xiaoqing Lin <lin@XiaoqingdeMacBook-Pro.local>
2018-09-23 15:14:30 -0400

    update ; mistake

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,8 @@
-name = "";
-def ask():
-    print("what is your question?");
-    name = input();
-    return name;
 
-name = ask();
+def ask():
+    print("what is your question?")
+    que= input()
+    return que
+
+question = ask();
+
