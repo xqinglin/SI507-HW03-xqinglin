962e99093a9c68797efef21b76ac1a864c1b9235
Xiaoqing Lin <lin@XiaoqingdeMacBook-Pro.local>
2018-09-23 15:37:53 -0400

    add new function to check if its is a question and loop till quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,8 +1,25 @@
 
 def ask():
     print("what is your question?")
-    que= input()
+    que = input()
     return que
 
-question = ask();
 
+def check_question(input):
+    size = len(input)
+    print(size)
+    if input[size-1] == '?':
+        return True
+    else:
+        print("I’m sorry, I can only answer questions")
+        return False
+
+
+def main():
+    question = ""
+    while question != "quit":
+        question = ask()
+        is_question = check_question(question)
+
+
+main()
